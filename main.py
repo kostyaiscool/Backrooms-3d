2254564a7dfe1218e01fcdbf9784cd37419e4538
kostyaiscool <yanubtitrupzaeal@gmail.com>
2024-01-03 15:48:49 +0200

    Не обязан

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from direct.showbase.ShowBase import ShowBase
 from mouseset import mouse
-from player import player
-
+from player import Player
+from map import Map
+from pausemenu import Menu, Pause
 class Game(ShowBase):
     def __init__(self):
         super().__init__()
-        self.mouse = mouse
-        self.player = player
-
+        self.menu = Menu()
+        self.pause = Pause()
+    def startGame(self):
+        # self.mouse = mouse
+        self.menu.panel.hide()
+        self.player = Player((0, 10, 0))
+        self.map = Map((0, 0, 0))
+    def stopGame(self):
+        # self.mouse = mouse
+        self.pause.panel.hide()
+        self.menu.panel.show()
 
 game = Game()
 game.run()
---
+++ pausemenu.py
@@ -0,0 +1,26 @@
+from direct.gui.DirectFrame import DirectFrame
+from direct.gui.DirectButton import DirectButton
+from sys import exit
+class Menu:
+    def __init__(self):
+        self.panel = DirectFrame(frameColor = (0, 0, 0, 0.5), frameSize = (-1, 1, -1, 1))
+        self.button1 = DirectButton(text='Start game', scale=0.2, parent=self.panel, command=base.startGame)
+        self.button2 = DirectButton(text='Quit', scale=0.2, parent=self.panel, command=exit)
+        self.button1.setPos(0, 0, 0.2)
+        self.button2.setPos(0, 0, -0.2)
+class Pause:
+    def __init__(self):
+        self.panel = DirectFrame(frameColor=(0, 0, 0, 0.5), frameSize=(-1, 1, -1, 1))
+        self.button1 = DirectButton(text='Continue', scale=0.2, parent=self.panel, command=self.unpause)
+        self.button2 = DirectButton(text='Main menu', scale=0.2, parent=self.panel, command=base.stopGame)
+        self.button1.setPos(0, 0, 0.2)
+        self.button2.setPos(0, 0, -0.2)
+        self.panel.hide()
+    def unpause(self):
+        self.panel.hide()
+        taskMgr.add(self.move, "move")
+        taskMgr.add(self.mouseTask, "mouseTask")
+    def pause(self):
+        self.panel.show()
+        taskMgr.remove("move")
+        taskMgr.remove("mouseTask")
